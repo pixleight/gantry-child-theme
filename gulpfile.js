22c2161821b1ceeb9a38ca6500a9218a96517b72
Chris Violette <cvioletterun@gmail.com>
2017-01-07 10:40:28 -0500

    updated gulpfile to move scss into blog-# directory only if declared in manifest.json

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,16 @@ var path = manifest.paths;
 // `config` - Store arbitrary configuration values here.
 var config = manifest.config || {};
 
+var blogpath = config.blogID ? 'blog-'+config.blogID : '';
+
 gulp.task('styles', function(){
   return gulp.src(['custom/scss/*.scss'])
-    .pipe(gulp.dest(path.dist + 'blog-' + config.blogID + '/scss'));
+    .pipe(gulp.dest(path.dist + blogpath + '/scss'));
 });
 
 gulp.task('images', function(){
   return gulp.src(['custom/images/**/*'])
-    .pipe(gulp.dest(path.dist + 'blog-' + config.blogID + '/images'));
+    .pipe(gulp.dest(path.dist + blogpath + '/images'));
 });
 
 gulp.task('watch', function(){
